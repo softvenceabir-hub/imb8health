e5976d7066d84edf5d10de95f7147ff6e03c6791
Sulayman-M2309 <abirstu9@gmail.com>
2025-08-30 13:12:37 +0600

    mogodbconnext

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        // console.log(process.env.MONGO_DB_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
+}
+module.exports = connectDB; 
--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,9 @@ const PORT = process.env.PORT || 4040
 // api routes
 //localhost:4000
 const router=require('./router')
+const connectDB = require('./config/db')
 app.use(router)
-
+connectDB()
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
