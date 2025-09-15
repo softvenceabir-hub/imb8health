4a434d1ff11521711eb7bedcc7751bf8905bd91c
Sulayman-M2309 <abirstu9@gmail.com>
2025-09-15 23:28:02 +0600

    class4

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,12 +1,14 @@
 const emailValidation = require("../helpers/emailValidation");
+const RandomOTP = require("../helpers/random_otp");
 const { sendEmail } = require("../helpers/sendEmail");
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
 
 const signupController = async (req, res) => {
-    const { username, email, password, phone } = req.body;
+    const { username, email, password } = req.body;
  try {
+        const otp = RandomOTP();
      bcrypt.hash(password, 10, async function(err, hash) {
         if (err) {
           return res.status(500).json({ success: false, error: err.message||"something went wrong" });
@@ -21,20 +23,27 @@ const signupController = async (req, res) => {
         username,
         email,
         password: hash,
-        phone
+        otp
      })
      await user.save();
-     sendEmail(email,"1234");
+     sendEmail(email, otp);
+   //   setTimeout(async () => {
+   //    await userModel.findOneAndUpdate({email},{otp: null}).then(()=>{
+   //       console.log("otp deleted");
+   //    })
+   //    user.save();
+   //   }, 2*60*1000); 
+
+   //   console.log("otp deleted");
      return res.status(201).json({ success: true,message: "User created successfully", data: user });
      }
     })
-   
- } catch (error) {
+   } catch (error) {
     return res.status(500).json({success: false,error: error.message || "something Wrong" });
  }
 }
 const loginController = async (req, res) => {
-    const { email, password } = req.body;
+
 }
 module.exports = {
   signupController,
---
+++ backend/helpers/random_otp.js
@@ -0,0 +1,5 @@
+function RandomOTP() {
+   const otp= Math.floor(100000 + Math.random() * 900000); 
+   return otp;
+}   
+module.exports = RandomOTP;
